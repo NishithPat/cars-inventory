494780b55503c1b2b653cf2193f6ad60121a732f
Nishith Patnaik <nishithpatnaik7@gmail.com>
2022-03-12 10:29:25 +0530

    updated index.js

--- back-end/index.js
+++ back-end/index.js
@@ -14,7 +14,7 @@ db.once('open', () => console.log('Connected to Database'))
 app.use(express.json())
 
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000")
+    res.header("Access-Control-Allow-Origin", "*")
     res.header(
         "Access-Control-Allow-Headers",
         "*" //"Origin, X-Requested-With, Content-Type, Accept, Authorization"
